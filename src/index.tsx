1374c3a2b1472402ecba26e82bebdf208d12faf7
InnaShamulina <innaaf@yandex.ru>
2024-10-09 13:07:44 +0300

    #3

--- src/index.tsx
+++ src/index.tsx
@@ -7,13 +7,15 @@ import store from './s2-homeworks/hw10/bll/store'
 import {Provider} from 'react-redux'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
+
 root.render(
-    <React.StrictMode>
-        {/*для дз 10*/}
+// @ts-ignore
+     <React.StrictMode>
+      {/*для дз 10*/}
         <Provider store={store}>
             <App/>
         </Provider>
-    </React.StrictMode>
+     </React.StrictMode>
 )
 
 // If you want to start measuring performance in your app, pass a function
